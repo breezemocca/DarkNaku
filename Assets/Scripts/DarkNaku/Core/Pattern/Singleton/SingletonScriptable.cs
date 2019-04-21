12ca3ace8801030c8ee4454a502af0a56961c472
skyrack <>
2019-04-22 04:53:54 +0900

    ScriptableObject Singleton 제네릭 클래스 추가

---
+++ Assets/Scripts/DarkNaku/Core/Pattern/Singleton/SingletonScriptable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace DarkNaku {
+	public abstract class SingletonScriptable<T> : ScriptableObject where T : ScriptableObject {
+		private const string RESOURCES_PATH = "Resources";
+
+		private static string _assetName = null;
+		private static string AssetName {
+			get {
+				if (string.IsNullOrEmpty(_assetName)) {
+					_assetName = string.Format("{0}Setting", typeof(T).ToString());
+				}
+
+				return _assetName;
+			}
+		}
+
+		private static T _instance = null;
+		public static T Instance { 
+			get {
+				if (_instance == null) {
+					_instance = Resources.Load(AssetName) as T;
+
+					if (_instance == null) {
+						_instance = CreateInstance<T>();
+#if UNITY_EDITOR
+						string resourcePath = System.IO.Path.Combine(Application.dataPath, RESOURCES_PATH);
+
+						if (System.IO.Directory.Exists(resourcePath) == false) {
+							AssetDatabase.CreateFolder("Assets", RESOURCES_PATH);
+						}
+
+						string fullPath = System.IO.Path.Combine(System.IO.Path.Combine("Assets", RESOURCES_PATH), AssetName + ".asset");
+						AssetDatabase.CreateAsset(_instance, fullPath);
+#endif
+					}
+				}
+
+				return _instance;
+			}
+		}
+	}
+}
